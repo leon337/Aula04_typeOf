0c0f5b906f23bc12859cae08bab7df6f1cff95e4
Leandro Carlos <leonpcsn@gmail.com>
2021-03-18 13:40:33 -0300

    Finalizando a Aula 04 typeOf

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 alert('Iniciei o JavaScript')
 
 
+// PARA SABER QUAL TIPO DE VARIAVEL EU TENHO
+
+
+
+const sorveteria = [];
+
+sorveteria[0] = 'Sorveteria_01';
+sorveteria[1] = 'Sorveteria_02';
+sorveteria[2] = 'Sorveteria_03';
+
+alert('A constante sorveteria é do tipo: ' + typeof(sorveteria));
+
+
+
+
 window.onload = function(){
 
 const lojas = {};
@@ -15,4 +30,5 @@ lojas.numero = 200;
 
 alert('O nome da loja é: ' + lojas.nome[0]);
 
+console.log(typeof(lojas.nome));
 }
